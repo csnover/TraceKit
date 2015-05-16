58848cc59b22309fc5558271d5eb6a15a1e6d7f8
Blake Niemyjski <bniemyjski@gmail.com>
2015-05-15 21:57:38 -0500

    Set anonymous method names as function signature when parsing jQuery Ajax stack traces
    Issue #14 (changes from raygun)

--- tracekit.js
+++ tracekit.js
@@ -965,6 +965,12 @@ TraceKit.computeStackTrace = (function computeStackTraceWrapper() {
                 item.func = parts[1];
             }
 
+            if (typeof item.func === 'undefined') {
+              try {
+                item.func = parts.input.substring(0, parts.input.indexOf('{'))
+              } catch (e) { }
+            }
+
             if ((source = findSourceByFunctionBody(curr))) {
                 item.url = source.url;
                 item.line = source.line;
