05693e04853701cf0a5d94f212e42b4417a33b28
Blake Niemyjski <bniemyjski@gmail.com>
2015-05-26 13:17:13 -0500

    Cleaned up old tests

--- tracekit-spec.js
+++ tracekit-spec.js
@@ -4,7 +4,29 @@
 (function() {
     'use strict';
 
-    describe('Tracing', function() {
+    describe('Handler', function(){
+        it('it should not go into an infinite loop', function (done){
+            var stacks = [];
+            function handler(stackInfo){
+                stacks.push(stackInfo);
+            }
+
+            function throwException() {
+                throw new Error('Boom!');
+            }
+
+            TraceKit.report.subscribe(handler);
+            expect(function () { TraceKit.wrap(throwException)(); }).toThrow();
+
+            setTimeout(function () {
+                TraceKit.report.unsubscribe(handler);
+                expect(stacks.length).toBe(1);
+                done();
+            }, 1000);
+        }, 2000);
+    });
+
+    describe('Parser', function() {
         function foo() {
             return bar();
         }
